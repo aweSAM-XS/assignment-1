22b8981097abe5e9cad9f23bada3ea5c10693fdf
Samuel Kiprop <sam.kiprop23@gmail.com>
2023-03-16 00:20:53 +0300

    Nice

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,11 @@
 const featuredImg = document.querySelector('#gallery figure img');
 const featuredCaption = document.querySelector('#gallery figure figcaption');
 const thumbnailContainer = document.getElementById('thumbnails');
-const heading = document.querySelector('#heading')
-const gallery = document.getElementById('gallery')
-const caption = document.getElementById('caption')
+const heading = document.querySelector('#heading');
+const gallery = document.getElementById('gallery');
+const caption = document.getElementById('caption');
 
-const imagesPath = '../images';
+const imagesPath = '/images';
 
 const imageData = [];
 const thumbnailItems = [];
@@ -15,11 +15,15 @@ fetch(imagesPath)
     .then((html) => {
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, 'text/html');
-        const files = Array.from(doc.querySelectorAll('a')).map((a) => a.textContent);
+        const files = Array.from(doc.querySelectorAll('a')).map(
+            (a) => a.textContent
+        );
         files.splice(0, 3);
         getImages(files);
     })
-    .catch((error) => console.log('Error getting directory information:', error));
+    .catch((error) =>
+        console.log('Error getting directory information:', error)
+    );
 
 const getImages = (files) => {
     const thumbnails = files.filter((file) => file.includes('small'));
@@ -30,10 +34,15 @@ const getImages = (files) => {
         const image = {
             thumb: name,
             full: name.replace('small', 'large'),
-            caption: name.split('-').slice(0, 2).reverse().join(' ').toUpperCase(),
+            caption: name
+                .split('-')
+                .slice(0, 2)
+                .reverse()
+                .join(' ')
+                .toUpperCase(),
         };
         imageData.push(image);
-        const thumbnailItem = `<img src="../images/${image.thumb}" alt="${image.caption}" class="thumbnail-img"/>`;
+        const thumbnailItem = `<img src="/images/${image.thumb}" alt="${image.caption}" class="thumbnail-img"/>`;
         thumbnailItems.push(thumbnailItem);
     });
 
@@ -41,11 +50,14 @@ const getImages = (files) => {
         const thumbnailElement = document.createElement('li');
         thumbnailElement.innerHTML = thumb;
         thumbnailElement.addEventListener('click', () => {
-            featuredImg.src = `../images/${imageData[index].full}` ;
+            featuredImg.src = `/images/${imageData[index].full}`;
             featuredImg.alt = imageData[index].caption;
             featuredCaption.textContent = imageData[index].caption;
-            gallery.style.background = `url(../images/${imageData[index].full}) no-repeat center / cover`;
-            caption.style.backgroundColor = imageData[index].caption.replace(' FLOWERS', '')
+            gallery.style.background = `url(/images/${imageData[index].full}) no-repeat center / cover`;
+            caption.style.backgroundColor = imageData[index].caption.replace(
+                ' FLOWERS',
+                ''
+            );
 
             thumbnailElements.forEach((thumb) => {
                 thumb.classList.remove('active');
